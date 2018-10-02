03d55f6375cf4ba08b1a241673edc3ce64d9e821
nalinahuja22 <42979014+nalinahuja22@users.noreply.github.com>
2018-10-02 18:13:18 -0400

    Update StringUtils.java

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -1,5 +1,8 @@
+/*@author: Nalin Ahuja*/
+
 public class StringUtils {
-    /**
+    
+	/**
      * makePurdueUsername -- <= 8-letter-long lowercase username creator
      * Usernames may be AT MOST 8 letters in length, and should
      * consist of the first letter of one's first name, and the 
@@ -7,22 +10,32 @@ public class StringUtils {
      * 7 characters. All should be lowercase.
      * Ex: a = "Sean ", b = "Flannery", returns "sflanner"
      */
+	
     public static String makePurdueUsername(String firstName, String lastName) {
-        /* Generate the long version of the name first:
+        
+    	/* Generate the long version of the name first:
             Ex: firstName = "Sean", lastName = "Flannery"
                     --> res = "SFlannery"
          */
         String res = firstName.substring(0,1) + lastName;
+       
         /* Shorten the string to 8 characters
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
+        
+        if((res.length() > 8))
+        {
+            res = res.substring(0, 8);
+        }
+        
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+        
+        res = res.toLowerCase();
+        
         /* return the result of our calculation */
         return res;
     }
@@ -34,8 +47,9 @@ public class StringUtils {
      * Ex: text = "jframes was among the students whose SSNs were leaked.",
      * username = "[DATA EXPUNGED] was among the students whose SSNs were leaked.",
      */
+    
     public static String replaceStudentUsername(String text, String username) {
-        /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNDGED]");
+
+    	return text.replaceAll(username, "[DATA EXPUNGED]");
     }
 }
